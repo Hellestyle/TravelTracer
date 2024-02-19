5bf34d63e19609fed9a93bb129f3111b6664d2a1
Simon Hellesylt-Larsen <simon@ipconnect.no>
2024-02-19 09:13:14 +0100

    added boilerplate

---
+++ website.py
@@ -0,0 +1,25 @@
+from flask import Flask, flash, render_template, request, redirect, url_for, session
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf import FlaskForm
+import secrets
+
+app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(16)
+
+#csrf = CSRFProtect(app)
+
+#loginManager = LoginManager()
+#loginManager.init_app(app)
+#loginManager.login_view = "/login"
+
+@app.route('/')
+def index():
+    return render_template("auth/login.hmtl")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
