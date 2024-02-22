282bb45d8df377272ec5365e06bffb046c089157
Isak Eriksen <eriksenly@icloud.com>
2024-02-22 21:37:26 +0100

    Squashed commit of the following:
    commit 105353e76c82a60b78dcb347372d825f85db341a
    Author: Tetiana <tda072@uit.no>
    Date:   Thu Feb 22 16:13:09 2024 +0100
    
        added SQL scripts for show, select, clear and drop all tables
    
    commit e6b3d9b090ee0e6dcb1177d7cd8899c1d42936ec
    Author: Tetiana <tda072@uit.no>
    Date:   Thu Feb 22 16:10:37 2024 +0100
    
        added SQL script, generated according to database model
    
    commit 31d1485745c9c3e9aa14f3afc67086a76a660373
    Author: Tetiana <tda072@uit.no>
    Date:   Thu Feb 22 16:08:06 2024 +0100
    
        renamed schema name mydb to stud_v23_she199
    
    commit d2488026c279433d8316c24cd815f63ccbb735db
    Author: Tetiana <tda072@uit.no>
    Date:   Thu Feb 22 16:05:57 2024 +0100
    
        updated .gitignore
    
    commit ef212f196f283ffd8b15cfb40d619e8b1aea95d9
    Author: Tetiana <tda072@uit.no>
    Date:   Thu Feb 22 13:12:05 2024 +0100
    
        created database model
    
    commit 41e3a967ecf7be9f718a293c0145eb8c4e0c2eea
    Merge: d1e6cd2 709b3c4
    Author: Simon Hellesylt-Larsen <simon@ipconnect.no>
    Date:   Thu Feb 22 11:20:35 2024 +0100
    
        Merge branch 'main' of https://github.com/Hellestyle/TravelTracer into main
    
    commit d1e6cd27ea10a8b33f8f6e033318bf4bb7f5aacf
    Author: Simon Hellesylt-Larsen <simon@ipconnect.no>
    Date:   Thu Feb 22 11:20:23 2024 +0100
    
        test
    
    commit 709b3c48b54460fe5d0cf2a2444a551e14c20503
    Author: Tetiana <tetianaverkhalantseva@narvik-byod-980.wifi.uit.no>
    Date:   Thu Feb 22 02:14:08 2024 +0100
    
        updated .gitignore
    
    commit 77250d1e4becea2fc44dc62e6c1cadc90792c7c8
    Author: Simon Hellesylt-Larsen <simon@ipconnect.no>
    Date:   Wed Feb 21 13:45:09 2024 +0100
    
        Added database kark
    
    commit 06a7e143c63c3028a82db010a9169240646f3ebe
    Author: Simon Hellesylt-Larsen <simon@ipconnect.no>
    Date:   Wed Feb 21 09:28:35 2024 +0100
    
        nothing
    
    commit d738ee79d1b86270817653c6376534c3581106a4
    Author: Simon Hellesylt-Larsen <simon@ipconnect.no>
    Date:   Wed Feb 21 09:24:35 2024 +0100
    
        Added unit-test
    
    commit 506c087483ba33fd017959be6fc02e32b0158e90
    Merge: 15634a0 716e568
    Author: Simon <57177898+Hellestyle@users.noreply.github.com>
    Date:   Wed Feb 21 09:16:18 2024 +0100
    
        Merge pull request #2 from Hellestyle/user-class
    
        User class rev0.1
    
    commit 716e568e6048ebb3bbd82db0897c516d60a3a8fa
    Author: Simon Hellesylt-Larsen <simon@ipconnect.no>
    Date:   Wed Feb 21 09:15:17 2024 +0100
    
        Added Database without creds and incomplete login
    
    commit a1cc6e59979d3f077744271332af3efaf652e459
    Author: Simon Hellesylt-Larsen <simon@ipconnect.no>
    Date:   Tue Feb 20 15:23:20 2024 +0100
    
        Added base userclass
    
    commit 15634a062a596c1c1431ef4c4e2679c1aef042c0
    Author: Simon <57177898+Hellestyle@users.noreply.github.com>
    Date:   Tue Feb 20 14:07:59 2024 +0100
    
        Create README.md
    
    commit 4a3b72ae07520d7742fb9658d4fae94fdd4d83b3
    Author: Viredon <155258644+Viredon@users.noreply.github.com>
    Date:   Mon Feb 19 23:25:11 2024 +0100
    
        Ie (#1)
    
        * test
    
        * optimized code
    
        * small fix
    
        * code
    
    commit 0323139934057432fea2e6e7ce28294fa7fab7ea
    Author: Isak Eriksen <eriksenly@icloud.com>
    Date:   Mon Feb 19 23:19:39 2024 +0100
    
        Squashed commit of the following:
    
        commit 06fa3be430fb4df044284582b066c269a693877b
        Author: Isak Eriksen <eriksenly@icloud.com>
        Date:   Mon Feb 19 23:13:00 2024 +0100
    
            code
    
        commit 61120e7a5cc4ac59cb918d7488705ac5ceca4d75
        Author: Isak Eriksen <eriksenly@icloud.com>
        Date:   Mon Feb 19 22:50:28 2024 +0100
    
            small fix
    
        commit 022c3b43bc3196f202d23c3c8716947594a1e217
        Author: Isak Eriksen <eriksenly@icloud.com>
        Date:   Mon Feb 19 22:10:26 2024 +0100
    
            optimized code
    
        commit 1a490cbaee293b0096d665436ec0cc2db30c9141
        Author: Isak Eriksen <eriksenly@icloud.com>
        Date:   Mon Feb 19 22:00:25 2024 +0100
    
            test
    
    commit 3aa1840b9f2adab0289f87cf3189909cfe0ea9bb
    Author: Isak Eriksen <eriksenly@icloud.com>
    Date:   Mon Feb 19 23:18:04 2024 +0100
    
        Update .gitignore

---
+++ database/ClearAllTables.sql
@@ -0,0 +1,34 @@
+USE `stud_v23_she199`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE achievement; 
+TRUNCATE TABLE achievement_meta; 
+TRUNCATE TABLE age_category;
+TRUNCATE TABLE city;
+TRUNCATE TABLE city_meta;
+TRUNCATE TABLE country;
+TRUNCATE TABLE country_meta;
+TRUNCATE TABLE friend;
+TRUNCATE TABLE `group`;
+TRUNCATE TABLE `language`;
+TRUNCATE TABLE page_element;
+TRUNCATE TABLE page_element_meta;
+TRUNCATE TABLE password_recovery;
+TRUNCATE TABLE sight;
+TRUNCATE TABLE sight_has_achievement;
+TRUNCATE TABLE sight_has_sight_type;
+TRUNCATE TABLE sight_meta;
+TRUNCATE TABLE sight_photo;
+TRUNCATE TABLE sight_type;
+TRUNCATE TABLE sight_type_meta;
+TRUNCATE TABLE `user`;
+TRUNCATE TABLE user_in_group;
+TRUNCATE TABLE user_system_meta;
+TRUNCATE TABLE verification;
+TRUNCATE TABLE visited_list;
+TRUNCATE TABLE wishlist;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+
---
+++ database/SelectAllTables.sql
@@ -0,0 +1,30 @@
+USE `stud_v23_she199`;
+
+select * from achievement; 
+select * from achievement_meta; 
+select * from age_category;
+select * from city;
+select * from city_meta;
+select * from country;
+select * from country_meta;
+select * from friend;
+select * from `group`;
+select * from `language`;
+select * from page_element;
+select * from page_element_meta;
+select * from password_recovery;
+select * from sight;
+select * from sight_has_achievement;
+select * from sight_has_sight_type;
+select * from sight_meta;
+select * from sight_photo;
+select * from sight_type;
+select * from sight_type_meta;
+select * from `user`;
+select * from user_in_group;
+select * from user_system_meta;
+select * from verification;
+select * from visited_list;
+select * from wishlist;
+
+
