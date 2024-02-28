6d4d6ce7683645b7c394e13b9ab620afc816470e
saf <svein.arne.fritsen@gmail.com>
2024-02-28 10:20:18 +0100

    custom validators complete + small changes to forms

--- forms.py
+++ forms.py
@@ -3,27 +3,33 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, InputRequired, EqualTo
 from wtforms import ValidationError
 
+from database import Database
+
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Length(3, 64), Email()])
+    email = StringField("Email", validators=[DataRequired(), Length(5, 40), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log in")
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Length(1, 64), Email()])
-    username = StringField("Username", validators=[DataRequired(), Length(1, 64)])
-    password = PasswordField("Password", validators=[DataRequired(), EqualTo("password2", message="Passwords must match.")])
+    email = StringField("Email", validators=[DataRequired(), Length(5, 40), Email()])
+    username = StringField("Username", validators=[DataRequired(), Length(2, 30)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8, message="Minimum 8 characters"), EqualTo("password2", message="Passwords must match.")])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     first_name = StringField("First name", validators=[InputRequired()])
     last_name = StringField("Last name", validators=[InputRequired()])
     submit = SubmitField('Register')
 
     def validate_email(self, field):
-        #Sqlspørring som sjekker om mail adressen allerede er i bruk
-            raise ValidationError("Email already registered")
+        with Database() as db:
+            email = field.data.lower()
+            if db.queryOne("SELECT * FROM user Where email = %s ", (email,)):
+                raise ValidationError("Email already registered")
     
     def validate_username(self, field):
-         #Sql spørring som sjekker om brukernavn allerede er registrert
-            raise ValidationError("Username already registered")
+        with Database() as db:
+            username = field.data
+            if db.queryOne("SELECT * FROM user Where username = %s ", (username,)):
+                raise ValidationError("Username already registered")
          
