9c195b2c75dfc268fbb261f7de7da76e43452074
Isak Eriksen <eriksenly@icloud.com>
2024-02-29 14:19:28 +0100

    Update website.py

--- website.py
+++ website.py
@@ -68,7 +68,7 @@ def sign_up():
             user = User()
             success, message = user.registrer(firstName, lastName, email, username, password)
             if success:
-                return f"{user}"
+                return f"Welcome {username}!"
             else:
                 flash(message)
                 return redirect(url_for("sign_up"))
