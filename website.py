b0f335f43fe57d6090ad5fc11d17139ac614467c
Tetiana <tda072@uit.no>
2024-03-06 13:31:49 +0100

    Created blueprint reglog (registration and login), moved logic for reglog from website.py to this blueprint

--- website.py
+++ website.py
@@ -7,6 +7,7 @@ from forms import LoginForm, RegistrationForm
 from user import User
 
 from sight.sight import sight
+from reglog.reglog import reglog
 
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ app.secret_key = secrets.token_urlsafe(16)
 csrf = CSRFProtect(app)
 
 app.register_blueprint(sight, url_prefix="/sight")
+app.register_blueprint(reglog, url_prefix="/reglog")
 
 
 #loginManager = LoginManager()
@@ -26,61 +28,6 @@ app.register_blueprint(sight, url_prefix="/sight")
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    
-    else:
-        loginForm = LoginForm(request.form)
-
-        if loginForm.validate():
-            email = loginForm.email.data
-            password = loginForm.password.data
-
-            user = User()
-            success, message = user.login(email, password)
-            if success:
-                return f"{user}"
-            else:
-                flash(message)
-                return render_template("login.html")
-        else:
-            for errors in loginForm.errors.values():
-                for error in errors:
-                    flash(error)
-            return redirect(url_for("login"))
-
-
-@app.route("/signup", methods=["POST", "GET"])
-def sign_up():
-    if request.method == "GET":
-        return render_template("signup.html")
-
-    else:
-        registrationForm = RegistrationForm(request.form)
-
-        if registrationForm.validate():
-            email = registrationForm.email.data
-            password = registrationForm.password.data
-            username = registrationForm.username.data
-            firstName = registrationForm.first_name.data
-            lastName = registrationForm.last_name.data
-
-            user = User()
-            success, message = user.registrer(firstName, lastName, email, username, password)
-            if success:
-                return f"Welcome {username}!"
-            else:
-                flash(message)
-                return render_template("signup.html")
-        else:
-            for errors in registrationForm.errors.values():
-                for error in errors:
-                    flash(error)
-            return redirect(url_for("sign_up"))
 
 
 if __name__ == "__main__":
