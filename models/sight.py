0151812a6e1343c56c7781254f50932b2ca2ec7e
Tetiana <tda072@uit.no>
2024-03-07 14:52:35 +0100

    Implemented getAllSights() method for getting info for all sights

--- models/sight.py
+++ models/sight.py
@@ -3,5 +3,103 @@ class Sight:
     def __init__(self, db):
         self.__db = db
 
-    def getAllSights(self):
-        return self.__db.query("SELECT * FROM sight;")
+    def getAllSights(self, language_id=None, country_id=None, city_id=None, age_category_id=None, sight_types=None, active_only=True):
+
+        if sight_types == []:
+            return []
+
+        if language_id is None:
+            language_id = self.__db.query("SELECT id FROM language WHERE `default` = 1;")[0]['id']
+
+        sights_query = """SELECT s.id AS id, sm.name AS name, sm.description AS description, cm.name AS city, ctrm.name AS country, s.latitude AS latitude, s.longitude AS longitude, s.active AS active, s.open_time AS open_time, s.close_time AS close_time,
+            ac.min_age AS min_age, ac.max_age AS max_age, stm.name AS sight_type,
+            (SELECT COUNT(*) FROM visited_list WHERE user_id IS NOT NULL AND sight_id = s.id) AS visited
+            FROM sight AS s
+            LEFT OUTER JOIN city AS c ON s.city_id = c.id
+            LEFT OUTER JOIN country AS ctr ON ctr.id = c.country_id
+            LEFT OUTER JOIN city_meta AS cm ON c.id = cm.city_id
+            LEFT OUTER JOIN country_meta AS ctrm ON ctr.id = ctrm.country_id
+            LEFT OUTER JOIN sight_meta AS sm ON s.id = sm.sight_id
+            LEFT OUTER JOIN age_category AS ac ON s.age_category_id = ac.id
+            LEFT OUTER JOIN sight_has_sight_type AS sst ON s.id = sst.sight_id
+            LEFT OUTER JOIN sight_type AS st ON st.id = sst.sight_type_id
+            LEFT OUTER JOIN sight_type_meta AS stm ON st.id = stm.sight_type_id
+            WHERE cm.language_id = %s AND sm.language_id = %s AND ctrm.language_id = %s AND stm.language_id = %s"""
+        
+        if active_only:
+            sights_query += " AND s.active = 1"
+        
+        parameters = [language_id, language_id, language_id, language_id]
+        
+        if age_category_id is not None:
+
+            sights_query += " AND ac.id = %s"
+
+            parameters.append(age_category_id)
+        
+        if country_id is not None and city_id is not None:
+
+            sights_query += " AND c.country_id = %s AND c.id = %s"
+        
+            parameters.extend([country_id, city_id])
+
+        elif country_id is not None:
+
+            sights_query += " AND c.country_id = %s"
+
+            parameters.append(country_id)
+
+        elif city_id is not None:
+
+            sights_query += " AND c.id = %s"
+
+            parameters.append(city_id)
+
+        if sight_types is not None:
+
+            sights_query += f" AND st.id IN ({','.join(['%s' for _ in sight_types])});"
+
+            parameters.extend(sight_types)
+
+        else:
+            sights_query += ";"
+
+        sight_records = self.__db.query(sights_query, parameters)
+
+        sights = {}
+
+        for sight_record in sight_records:
+
+            sight_id = sight_record['id']
+
+            if sight_id not in sights:
+                sights[sight_id] = {
+                    'id': sight_id,
+                    'name': sight_record['name'],
+                    'description': sight_record['description'],
+                    'city': sight_record['city'],
+                    'country': sight_record['country'],
+                    'latitude': sight_record['latitude'],
+                    'longitude': sight_record['longitude'],
+                    'active': sight_record['active'],
+                    'open_time': sight_record['open_time'],
+                    'close_time': sight_record['close_time'],
+                    'min_age': sight_record['min_age'],
+                    'max_age': sight_record['max_age'],
+                    'sight_types': []
+                }
+
+            sights[sight_id]['sight_types'].append(sight_record['sight_type'])
+
+        sights = list(sights.values())
+
+        for i in range(len(sights)):
+
+            sights[i]['achievements'] = self.__db.query("""SELECT a.*, am.name AS name, am.description AS description FROM achievement AS a
+                LEFT OUTER JOIN sight_has_achievement AS sa ON a.id = sa.achievement_id
+                LEFT OUTER JOIN achievement_meta AS am ON a.id = am.achievement_id
+                WHERE sa.sight_id = %s AND am.language_id = %s;""", (sights[i]['id'], language_id))
+            
+            sights[i]['photos'] = [photo['photo'] for photo in self.__db.query("SELECT photo FROM sight_photo WHERE sight_id = %s;", (sights[i]['id'],))]
+
+        return sights
