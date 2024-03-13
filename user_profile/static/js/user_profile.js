3d83d3afff54b62bf6144cf205c2049f20192d82
Asle-Oren <asleoren@gmail.com>
2024-03-13 18:11:25 +0100

    Added javascript code to hide/show content correctly. Cleaned up unused css properties.

--- user_profile/static/js/user_profile.js
+++ user_profile/static/js/user_profile.js
@@ -1,45 +1,65 @@
-const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+const sideBarMenuOptions = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+const contentDivs = document.querySelectorAll('.content > div');
 
-sideLinks.forEach(item => {
+sideBarMenuOptions.forEach(item => {
     const li = item.parentElement;
     item.addEventListener('click', () => {
-        if (sideBar.classList.contains('close')) {
-            
-        } else {
-            sideLinks.forEach(i => {
-                i.parentElement.classList.remove('active');
-            })
-            li.classList.add('active');
-            sideBar.classList.toggle('close');
-        }
-    })
+        sideBarMenuOptions.forEach(i => {
+            i.parentElement.classList.remove('active');
+        });
+        const itemid = item.id;
+        contentDivs.forEach(b => {
+            if (b.id === itemid){
+                b.classList.remove('hidden');
+            } else {
+                b.classList.add('hidden');
+            }
+        });
+        li.classList.add('active');
+        sideBar.classList.toggle('close');
+    });
 });
 
 const menuBar = document.querySelector('.content nav .bx.bx-menu-alt-left');
 const sideBar = document.querySelector('.sidebar');
-sideBar.classList.toggle('close');
-
 menuBar.addEventListener('click', () => {
     sideBar.classList.toggle('close');
 });
 
+const settingsButtons = document.querySelectorAll('.content div#settings > button');
+const settingsDivs = document.querySelectorAll('.content div#settings > div');
+console.log(settingsButtons);
+console.log(settingsDivs);
+settingsButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        settingsDivs.forEach(div => {
+            if (div.id === button.id) {
+                div.classList.toggle('hidden');
+            } else {
+                div.classList.add('hidden');
+            };
+        });
+    });
+});
+
+//not currently used
 function toggleDiv(id) {
       var settingsDiv = document.getElementById(id);
       // Toggle between 'none' and 'block' for display property
       settingsDiv.style.display = (settingsDiv.style.display === 'none' || settingsDiv.style.display === '') ? 'block' : 'none';
-    }
+    };
 
 //not currently used
 function openSidebar(id) {
     document.getElementById("sidebar").style.width = "250px";
     document.getElementById(id).style.marginLeft = "250px";
-}
+};
 //not currently used
 function closeSidebar(id) {
     document.getElementById(id).style.width = "0";
     document.getElementById(id).style.marginLeft= "0";
 }
-
+//not currently used
 function toggleDivs(divIds) {
     // Hide all divs initially
     divIds.forEach(function(id) {
