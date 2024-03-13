9ea4abc3720d2be6ae44cbd65d5d3a61e6013499
Asle-Oren <asleoren@gmail.com>
2024-03-13 11:55:55 +0100

    Fixed eventlisteners. Fixed sidebar menu button.

--- user_profile/static/js/user_profile.js
+++ user_profile/static/js/user_profile.js
@@ -3,15 +3,21 @@ const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logou
 sideLinks.forEach(item => {
     const li = item.parentElement;
     item.addEventListener('click', () => {
-        sideLinks.forEach(i => {
-            i.parentElement.classList.remove('active');
-        })
-        li.classList.add('active');
+        if (sideBar.classList.contains('close')) {
+            
+        } else {
+            sideLinks.forEach(i => {
+                i.parentElement.classList.remove('active');
+            })
+            li.classList.add('active');
+            sideBar.classList.toggle('close');
+        }
     })
 });
 
 const menuBar = document.querySelector('.content nav .bx.bx-menu-alt-left');
 const sideBar = document.querySelector('.sidebar');
+sideBar.classList.toggle('close');
 
 menuBar.addEventListener('click', () => {
     sideBar.classList.toggle('close');
@@ -22,3 +28,14 @@ function toggleDiv(id) {
       // Toggle between 'none' and 'block' for display property
       settingsDiv.style.display = (settingsDiv.style.display === 'none' || settingsDiv.style.display === '') ? 'block' : 'none';
     }
+
+//not currently used
+function openSidebar(id) {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById(id).style.marginLeft = "250px";
+}
+//not currently used
+function closeSidebar(id) {
+    document.getElementById(id).style.width = "0";
+    document.getElementById(id).style.marginLeft= "0";
+}
