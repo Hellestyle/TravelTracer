358323d9a0929449518b433e5bc657cdf3dc01f8
saf <svein.arne.fritsen@gmail.com>
2024-04-02 13:59:58 +0200

    menu closes when clicked outside + menu close icon

--- static/js/mobile_menu.js
+++ static/js/mobile_menu.js
@@ -1,8 +1,29 @@
-function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav-container") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav-container";
-    }
+function toggleMenu() {
+  var x = document.getElementById("nav");
+  var icon = document.querySelector('.nav-container a.icon i');
+
+  if (x.className === "nav-container") {
+    x.className += " responsive";
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-times');
+  } else {
+    x.className = "nav-container";
+    icon.classList.add('fa-bars');
+    icon.classList.remove('fa-times');
   }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('click', function (event) {
+    var navContainer = document.getElementById('nav');
+    var menuIcon = document.querySelector('.nav-container a.icon');
+
+    if (menuIcon.contains(event.target)) {
+      toggleMenu();
+    } else if (!navContainer.contains(event.target) && navContainer.classList.contains('responsive')) {
+      navContainer.classList.remove('responsive');
+      document.querySelector('.nav-container a.icon i').classList.add('fa-bars');
+      document.querySelector('.nav-container a.icon i').classList.remove('fa-times');
+    }
+  });
+});
