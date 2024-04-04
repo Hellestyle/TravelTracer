7e53c94679027cce59eb5a05667a4659fdaf7188
Isak Eriksen <eriksenly@icloud.com>
2024-04-04 22:11:51 +0200

    Added edit_sight.html

--- admin/admin.py
+++ admin/admin.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from database import  Database
 from models.sight_name import SightName
 from models.sight_location import Sight_location
 from forms import EditSight
 from models.sight import Sight
 from models.sight_type import SightType
+from datetime import datetime as dt
+
 
 admin = Blueprint("admin", __name__, template_folder="templates", static_folder="static")
 
@@ -29,8 +31,18 @@ def admin_main():
     )
 
 
-@admin.route("/id/<int:sight_id>")
+@admin.route("/sight/id/<int:sight_id>")
 def edit_sight(sight_id):
-    
-    edit_sight_form = EditSight()
-    return render_template("edit_sight.html")
+    with Database(dict_cursor=True) as db:
+        sight_model = Sight(db)
+        sight = sight_model.getSight(sight_id)
+
+    if sight is not None:
+        now = dt.now().time()
+        is_open = sight["open_time"] is None and sight["close_time"] is None or \
+            sight["open_time"] <= now and sight["close_time"] >= now
+
+        return render_template(
+            "edit_sight.html",
+            sight=sight
+        )
