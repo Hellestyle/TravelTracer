e785cca2f54d62d3627c18f97b08395dcfa0d984
saf <svein.arne.fritsen@gmail.com>
2024-04-19 14:09:14 +0200

    added form for sight type

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField, MultipleFileField,SelectField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField, MultipleFileField,SelectField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, InputRequired, EqualTo
 from wtforms import ValidationError
 from database import Database
@@ -114,6 +114,13 @@ class Edit_acheivements(FlaskForm):
     desc = StringField("Description",validators=[DataRequired()])
     image = FileField("Image",validators=[])
     submit = SubmitField("Submit")
+
+class EditOrAddSightType(FlaskForm):
+    name = StringField("Name",validators=[DataRequired()])
+    desc = StringField("Description",validators=[DataRequired()])
+    points = IntegerField("Points",validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
         
 if __name__ == "__main__":
