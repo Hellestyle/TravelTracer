b4a920eff902e95ce0c4b717adc5e93dd95e0d44
Simon <57177898+Hellestyle@users.noreply.github.com>
2024-05-23 17:00:42 +0200

    Update mariadb.py

--- config/mariadb.py
+++ config/mariadb.py
@@ -1,4 +1,4 @@
-HOST = 'kark.uit.no'
-USER = 'stud_v23_she199'
-PASSWORD = 'Q7TOeuhlLTSj2lfT'
-DATABASE = 'stud_v23_she199'
+HOST = 'CHANGEME'
+USER = 'CHANGEME'
+PASSWORD = 'CHANGEME'
+DATABASE = 'CHANGEME'
